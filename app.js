0586266c6ecc4b21c6a7f51747bea54b02a25131
TintinCoder <81453801+TintinCoder@users.noreply.github.com>
2021-10-02 15:06:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,11 +12,35 @@ function startAudio() {
     setTimeout(stopGif, 2000);
 
     navigator.mediaDevices.getUserMedia({audio: true});
-    var classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/0jnux8wSi/model.json', modelReady)
 }
+var classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/ElIAPE6Ip/model.json', modelReady)
 function modelReady() {
     classifier.classify(gotResult)
 }
 function gotResult(error, result) {
-    console.log('got result')
+    if (error) {
+        console.log(error)   
+    }
+    else {
+        const number_r = Math.floor(Math.random() * 255) + 1;
+        const number_b = Math.floor(Math.random() * 255) + 1;
+        const number_g = Math.floor(Math.random() * 255) + 1;
+        // console.log(result)
+        document.getElementById('detectedVoice').innerHTML = result[0].label;
+        document.getElementById('detectedVoiceAccuracy').innerHTML = ((result[0].confidence) * 100 + 1).toFixed(2) + " %";
+        document.getElementById('detection').style.color = "rgb(" + number_r + "," + number_g + "," + number_b + ")"
+
+        if (result[0].label == "Barking") {
+            document.getElementById('hearing').style.display = "block";
+            document.getElementById('hearing').src = 'bark.gif';
+        }
+        else if (result[0].label == "Meow") {
+            document.getElementById('hearing').style.display = "block";
+            document.getElementById('hearing').src = 'meow.gif';
+        }
+        else {
+            document.getElementById('hearing').style.display = "block";
+            document.getElementById('hearing').src = 'bn.gif';
+        }
+    }
 }
